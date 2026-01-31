5aecc50e7c593cd500dc6a628c129c30e16c33a1
Joaquin Labadie <labadiejoaco.accs@gmail.com>
2026-01-30 23:05:02 -0300

    fix: align SidebarTrigger in app header

--- components/app-header.tsx
+++ components/app-header.tsx
@@ -5,7 +5,7 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 export function AppHeader() {
   return (
     <header className="bg-sidebar border-sidebar-border sticky top-0 flex h-16 items-center border-b px-4">
-      <SidebarTrigger className="size-5" />
+      <SidebarTrigger className="-ml-1" />
     </header>
   );
 }
